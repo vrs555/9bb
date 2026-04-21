5b504e8cb16abbfad81b0b8ac3d1ad5a6648ef00
vrs555 <vrs09055@gmail.com>
2026-04-21 13:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("hello, this is from github")
+balance=3000
+print("initial balance ", balance)
+balance=balance-500
+print("balance after 500 deduction ", balance)
+balance=balance+1000
+print("balance after 1000 deposition ", balance)
